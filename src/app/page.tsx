0df6dde52b581a4659ff9ab544f89570f931fddf
k1ego <ladonin05@mail.ru>
2024-08-14 16:28:24 +0300

    Added filtering bar and sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
 	return (
@@ -6,24 +6,23 @@ export default function Home() {
 			<Container className='mt-10'>
 				<Title text='Все пиццы' size='lg' className='font-extrabold' />
 			</Container>
-      <TopBar />
 
-      {/* Основная часть */}
-      <Container className='mt-10 pb-14'>
-        <div className='flex gap-[60px]'>
+			<TopBar />
 
-          {/* Фильтрация */}
-          <div className='w-[250px]'>
-            <Filters />
-          </div>
+			{/* Основная часть */}
+			<Container className='mt-10 pb-14'>
+				<div className='flex gap-[60px]'>
+					{/* Фильтрация */}
+					<div className='w-[250px]'>
+						<Filters />
+					</div>
 
-          {/* Список товаров */}
-          <div className='flex-1'>
-            <div className='flex flex-col gap-16'>Список товаров</div>
-          </div>
-        </div>
-      </Container>
-
+					{/* Список товаров */}
+					<div className='flex-1'>
+						<div className='flex flex-col gap-16'>Список товаров</div>
+					</div>
+				</div>
+			</Container>
 		</>
 	);
 }
