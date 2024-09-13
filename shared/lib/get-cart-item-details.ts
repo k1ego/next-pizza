91764bce76ba3c0d3ae5c24fd65b7d98f2165ba0
k1ego <ladonin05@mail.ru>
2024-09-13 23:59:38 +0300

    Сделано подтягивание реальных данных о товарах с бекенда

--- shared/lib/get-cart-item-details.ts
+++ shared/lib/get-cart-item-details.ts
@@ -1,10 +1,11 @@
 import { Ingredient } from '@prisma/client';
 import { mapPizzaType, PizzaSize, PizzaType } from '../constants/pizza';
+import { CartStateItem } from './get-cart-details';
 
 export const getCartItemDetails = (
+  ingredients: CartStateItem['ingredients'],
 	pizzaType: PizzaType,
 	pizzaSize: PizzaSize,
-	ingredients: Ingredient[],
 ): string => {
 
 	const details = [];
